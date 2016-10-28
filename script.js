ceaaa305e816b6cdc930d0a9c5e153f3364d927b
graholm <graholm@nuevaschool.org>
2016-10-28 09:06:26 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 console.log("time to add some firebase code!")
+var obj1 = {
+  nestedkey1: {
+      nestedkey2: {
+      nestedkey
+    }
+}
+
 
 var obj = {
   key1: "jols!!",
   key2: "adios",
   key3: "rdtfyghb",
-  key4: {
-    key7: "
-  }
+ 
 }
 
 addListItem("listKey1", obj); 
